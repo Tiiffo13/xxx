e6743947395818077f7b733250696931bb4ecfb1
Tiiffo132 <forengatets@gmail.com>
2023-12-11 18:11:22 +0300

    123
    123

--- сайт/RepairPlus/obj/Debug/net6.0/RepairPlus.AssemblyInfo.cs
+++ сайт/RepairPlus/obj/Debug/net6.0/RepairPlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepairPlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4261db7a93074776c665594d16c4fae2f62f0b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepairPlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepairPlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
